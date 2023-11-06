3fcf9f1bcedea2900d4282aa274c9606c0edbb25
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-11-06 16:45:32 +0530

    installed react-dom

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Container,Navbar} from 'react-bootstrap'
+const Header = () => {
+  return (
+    <Navbar bg='dark' variant="dark" style={{height:"50px"}}>
+     <Container>
+      <Navbar.Brand>
+       <Link>
+       
+       </Link>
+      </Navbar.Brand>
+     </Container>
+    </Navbar>
+  )
+}
+
+export default Header
