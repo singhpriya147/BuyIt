efef61a0fc7857eb583afb8178fba4d964a743a1
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-11-08 20:46:51 +0530

    added buttons

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import {Card} from "react-bootstrap"
+import {Button} from 'react-bootstrap';
 import './style.css';
+import { CartContext } from '../context/Context';
+
+
+import {CartState} from '../context/Context'
 const Product = ({SingleProduct}) => {
+
+// const {state:{cart},dispatch}=CartState()
+  const { state:{cart},dispatch } = useContext(CartContext);
+
+
   return (
     <div className='products'>
       <Card>
@@ -17,10 +27,21 @@ const Product = ({SingleProduct}) => {
                 Regular Price: {price.regularPrice.minPrice}
                 {price.regularPrice.maxPrice}$
               </p>
-             
             </div>
           ))}
         </Card.Subtitle>
+
+        {cart.some((p) => p.webID === SingleProduct.webID) ? (
+          <Button variant='danger' onClick={()=>{dispatch({
+           type:'REMOVE_FROM_CART',
+           payload:SingleProduct,
+          })}}>Remove from cart</Button>
+        ) : (
+          <Button onClick={()=>{dispatch({
+            type: 'ADD_TO_CART',
+            payload: SingleProduct,
+          });}}>Add to cart</Button>
+        )}
       </Card>
     </div>
   );
