070b77c3269ddf28889a3ad7fc74a835fc175340
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-11-27 01:06:35 +0530

    remove unused comment

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
-// import { images } from '../assets/images/carouselData';
+
 import {images} from '../assets/images/carouselData'
 import './Carousel.css';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa6';
@@ -11,26 +11,21 @@ const Carousel = ({autoSlide=false,autoSlideInterval=3000}) => {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrImg((prevImg) => (prevImg + 1) % images.length);
-    }, 5000); // Change the interval duration (in milliseconds) as needed
+    }, 5000); 
 
-    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
+    return () => clearInterval(intervalId); 
   }, []);
 
-// useEffect(()=>{
-// if(!autoSlide)return 
-// const slideInterval(setInterval() => {
-  
-// }, interval);
-// },[])
+
   return (
     <div className='carousel'>
       <div
         className='carouselInner'
         style={{
           backgroundImage: `url(${images[currImg].img})`,
-          // transition: 'background-image 3s ease-in-out',
+          
            transition: 'background-image 3s ease-in-out, opacity 1s ease-in-out, transform 0.5s ease-in-out'
-          // transform:`translate(-${currImg*100}%)`
+         
         }}
         
       >
